782f54a2d179bbf122b23ff1dc7f2ce1b09a9552
Mylo Cyrus <irahiggins740@gmail.com>
2023-07-18 10:49:32 -0700

    Add files via upload

---
+++ current_DDOS.clj
@@ -0,0 +1,299 @@
+Reply from 104.18.5.159: bytes=32 time=58ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 58ms, Maximum = 129ms, Average = 86ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=64ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=388ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=103ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=73ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 64ms, Maximum = 388ms, Average = 157ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=56ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=28ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=73ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=41ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 28ms, Maximum = 73ms, Average = 49ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=86ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=52ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=517ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=45ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 45ms, Maximum = 517ms, Average = 175ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=39ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=41ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=36ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=69ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 36ms, Maximum = 69ms, Average = 46ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=48ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=41ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=42ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=83ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 41ms, Maximum = 83ms, Average = 53ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=43ms TTL=53
+Request timed out.
+Request timed out.
+Request timed out.
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 1, Lost = 3 (75% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 43ms, Maximum = 43ms, Average = 43ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Request timed out.
+Reply from 104.18.5.159: bytes=32 time=29ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=201ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=40ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 3, Lost = 1 (25% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 29ms, Maximum = 201ms, Average = 90ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=133ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=65ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=104ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=85ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 65ms, Maximum = 133ms, Average = 96ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=153ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=64ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=33ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=895ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 33ms, Maximum = 895ms, Average = 286ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=91ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=84ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=44ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=109ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 44ms, Maximum = 109ms, Average = 82ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=76ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=47ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=40ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=88ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 40ms, Maximum = 88ms, Average = 62ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=79ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=84ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=387ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=74ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 74ms, Maximum = 387ms, Average = 156ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=51ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=87ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=32ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=47ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 32ms, Maximum = 87ms, Average = 54ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=36ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=41ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=435ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=98ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 36ms, Maximum = 435ms, Average = 152ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=34ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=62ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=56ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=111ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 34ms, Maximum = 111ms, Average = 65ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=47ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=46ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=1170ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=96ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 46ms, Maximum = 1170ms, Average = 339ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=86ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=47ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=96ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=92ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 47ms, Maximum = 96ms, Average = 80ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=54ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=115ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=88ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=48ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 48ms, Maximum = 115ms, Average = 76ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=44ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=274ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=36ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=150ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 36ms, Maximum = 274ms, Average = 126ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=40ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=105ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=55ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=76ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 40ms, Maximum = 105ms, Average = 69ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=63ms TTL=53
+Request timed out.
+Reply from 104.18.5.159: bytes=32 time=47ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=78ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 3, Lost = 1 (25% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 47ms, Maximum = 78ms, Average = 62ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=64ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=44ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=109ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=1288ms TTL=53
+
+Ping statistics for 104.18.5.159:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 44ms, Maximum = 1288ms, Average = 376ms
+0
+---------
+
+Pinging 104.18.5.159 with 32 bytes of data:
+Reply from 104.18.5.159: bytes=32 time=87ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=70ms TTL=53
+Reply from 104.18.5.159: bytes=32 time=57ms TTL=53
